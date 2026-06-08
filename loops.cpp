d672a6010c44f0ae9f5533d419e6708f01c9627c
Ishan Chauhan <2k25cse2510719@gmail.com>
2026-06-08 13:05:05 +0530

    1 To 100 series
    Series Using Loops

---
+++ loops.cpp
@@ -0,0 +1,12 @@
+//Series Printing using loops 
+#include<iostream>
+using namespace std;
+int main()
+{ int a ;
+    cout<<"Enter Number of terms";
+    cin>>a;
+for(int i =0;i<=a;i++)
+{
+    cout<<i<<endl;
+}
+}
